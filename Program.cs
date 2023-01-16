e4d90a313a9bbc5f5970923e813925c7b06877ae
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-16 12:23:42 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Regestration Program by Regex");
+           
+        }
+       
+    }
+}
+
